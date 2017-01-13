bdecf89ac70cd7b8fe3fa97443d5df37ea7a18db
Daniel H <dheffner2010@gmail.com>
2017-01-13 12:42:14 -0600

    basic views.py setup

--- reviews/apps/review_api/views.py
+++ reviews/apps/review_api/views.py
@@ -1,4 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.urlresolvers import reverse
+from djang0.http import JsonResponse
 
-def index(req):
-	return render(req, 'review_api/index.html')
+def index(request):
+	return render(request, 'review_api/index.html')
+
+def register(request):
+	if request.method == "POST":
+		placeholder = {is_placeholder: True}
+		return JsonResponse(placeholder)
+	else:
+		return redirect(reverse('index'))
+
+def create(request):
+	if request.method == "POST":
+		placeholder = {is_placeholder: True}
+		return JsonResponse(placeholder)
+	else:
+		return redirect(reverse('index'))
+
+def retrieve(request):
+	placeholder = {is_placeholder: True}
+	return JsonResponse(placeholder)
+
