3024807f16e2f0d5f3330efb3580495c62cff58c
Daniel H <dheffner2010@gmail.com>
2017-01-13 10:10:09 -0600

    added find/create manager for companies

--- reviews/apps/review_api/models.py
+++ reviews/apps/review_api/models.py
@@ -2,9 +2,21 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+class CompanyManager(models.Manager):
+	def findOrCreate(self, cname):
+		comp = Company.objects.filter(name=cname)
+		if comp[0]:
+			return comp[0]
+		else:
+			comp = Company(name=cname)
+			comp.save()
+			comp = Company.objects.filter(name=cname)
+			return comp[0]
+
 class Company(models.Model):
 	name = models.CharField(max_length=100)
 	created_at = models.DateTimeField(auto_now_add=True)
+	objects = CompanyManager()
 
 class Reviewer(models.Model):
 	first_name = models.CharField(max_length=200)
@@ -12,7 +24,7 @@ class Reviewer(models.Model):
 	email = models.EmailField()
 	password = models.CharField(max_length=200)
 	created_at = models.DateTimeField
-	
+
 class Review(models.Model):
 	ratings = models.PositiveSmallIntegerField()
 	title = models.CharField(max_length=64)
