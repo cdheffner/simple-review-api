6c2a32f1635e3bc6051c6895fb5c0a58d3edee8b
Daniel H <dheffner2010@gmail.com>
2017-01-13 12:21:02 -0600

    added urls

--- reviews/apps/review_api/urls.py
+++ reviews/apps/review_api/urls.py
@@ -2,5 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index)
+	url(r'^$', views.index, name='index'),
+	url(r'^register$', views.register, name='register'),
+	url(r'^submit$', views.create, name='create'),
+	url(r'^api/retrieve$', views.retrieve, name='retrieve'),
 ]
